1f8305a759e2f526b7e4f34a944cc164ed3e1602
elbegsaikhan.a <elbegsaikhana.a@khanbank.com>
2024-04-22 14:20:17 +0800

    add assioted domain

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/.well-known/apple-app-site-association', function(req, res, next) {
  res.send(
    {
      applinks: {},
